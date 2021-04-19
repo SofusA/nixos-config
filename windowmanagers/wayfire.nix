ac1588d678cc4113cf17e3f67ff59cddc430d7dc
Sofus <soa@eupry.com>
2021-04-19 11:37:28 +0200

    wayfire?

--- windowmanagers/wayfire.nix
+++ windowmanagers/wayfire.nix
@@ -1,20 +1,68 @@
 { config, lib, pkgs, ... }:
-let
-  rev = "master"; # could be a git rev, to pin the overlay (not usually recommended)
-  url = "https://github.com/colemickens/nixpkgs-wayland/archive/${rev}.tar.gz";
-  waylandOverlay = (import (builtins.fetchTarball url));
-in
-  {
-    nixpkgs.config.permittedInsecurePackages = [
-      "p7zip-16.02"
-    ];
 
-    nixpkgs.overlays = [ waylandOverlay ];
-    environment.systemPackages = with pkgs; [
-    	wayfire 
-    ];
-    
-    home-manager.users.sofusa = {
-    	xdg.configFile."wayfire.ini".source = ./wayfire.ini;
+{
+  
+  environment.systemPackages = with pkgs; [ 
+    wayfire
+    polkit_gnome # polkit show
+  ];
+
+  environment.pathsToLink = [ "/libexec" ]; # polkit show
+
+  # pipewire stuff
+  security.rtkit.enable = true;
+  services.pipewire = {
+      enable = true;
+      pulse.enable = true;
+  };
+
+  # Bluetooth
+  services.blueman.enable = true;
+
+  # Auto log in
+  # services.getty.autologinUser = "sofusa";
+  # environment.loginShellInit = ''
+  # [[ "$(tty)" == /dev/tty1 ]] && sway
+  #     '';
+
+  # Dotfiles
+    environment = {
+      etc = {
+        "ranger/rc.conf".source = ../dotfiles/ranger/rc.conf;
+      };
     };
-  }
+
+    home-manager.users.sofusa = {
+        xdg.configFile = {
+            "kak".source = ../dotfiles/kak;
+            "waybar".source = ../dotfiles/waybar;
+            "wofi".source = ../dotfiles/wofi;
+            "kitty".source = ../dotfiles/kitty;
+            "wayfire.ini".source = ./wayfire.ini;
+        };
+        gtk = {
+            enable = true;
+
+            iconTheme = {
+                name = "Papirus";
+                package = pkgs.papirus-icon-theme;
+            };
+            
+            theme = {
+                name = "Pop";
+                package = pkgs.pop-gtk-theme;
+            };
+
+            font = {
+                package = pkgs.roboto;
+                name = "Roboto 11";
+            };
+        };
+    };
+
+  environment.variables = {
+      MOZ_ENABLE_WAYLAND = "1";
+      XDG_CURRENT_DESKTOP = "sway";
+      XDG_SESSION_TYPE = "wayland";
+  };
+}
